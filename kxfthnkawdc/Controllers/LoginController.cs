0f38046c54f2cd4b40b64d3e6feb068113ee7a88
Andrey Modnov <89213188577@mail.ru>
2023-10-10 16:50:31 +0300

    Login process impoved and registration implemented

--- kxfthnkawdc/Controllers/LoginController.cs
+++ kxfthnkawdc/Controllers/LoginController.cs
@@ -8,35 +8,105 @@ using Npgsql;
 namespace kxfthnkawdc.Controllers;
 
 [Route("login")]
+[ApiController]
 public class LoginController : ControllerBase
 {
-    private readonly ILogger<LoginController> _logger;
     private readonly ApplicationDbContext _dbContext;
 
-    public LoginController(ILogger<LoginController> logger, ApplicationDbContext dbContext)
+    public LoginController(ApplicationDbContext dbContext)
     {
-        _logger = logger;
         _dbContext = dbContext;
     }
 
-    [HttpPost]
-    [Route("verify")]
-    public ActionResult Login([FromHeader] string username, [FromHeader] string password)
+    private bool IsAlreadyLogined => Request.HttpContext.Session.GetInt32("id") > 0;
+
+    private void CreateUserSession(int id, string username)
     {
+        HttpContext.Session.SetInt32("id", id);
+        HttpContext.Session.SetString("username", username.ToLower());
+        HttpContext.Session.CommitAsync().Wait();
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public string Login([FromHeader] string username, [FromHeader] string password)
+    {
+        if (IsAlreadyLogined)
+            return "Success";
+
         using var command =
             _dbContext.DataSource.CreateCommand(
-                "select * from users where name=(@username) and password=(@password)");
+                "SELECT * FROM users WHERE name=(@username) AND password=(@password) LIMIT 1");
         command.Parameters.AddWithValue("username", username.ToLower());
         command.Parameters.AddWithValue("password", password);
         using var reader = command.ExecuteReader();
         if (reader.Read())
         {
-            HttpContext.Session.SetInt32("id", (int)reader["id"]);
-            HttpContext.Session.SetString("username", username.ToLower());
-            HttpContext.Session.CommitAsync();
-            return RedirectPermanent("/chat.html");
+            CreateUserSession((int)reader["id"], username);
+            return "Success";
         }
 
-        return new UnauthorizedResult();
+        return "Wrong username or password";
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public string Register([FromHeader] string username, [FromHeader] string password)
+    {
+        if (IsAlreadyLogined)
+            return "Success";
+
+        if (UserAlreadyExist())
+            return "Success";
+
+        if (UsernameIsAvailable() == false)
+            return "This username is being used by another user";
+
+        CompleteRegistration();
+        return "Success";
+
+        bool UserAlreadyExist()
+        {
+            using var command =
+                _dbContext.DataSource.CreateCommand(
+                    "SELECT id, name FROM users WHERE name=(@username) AND password=(@password)");
+            command.Parameters.AddWithValue("username", username.ToLower());
+            command.Parameters.AddWithValue("password", password);
+            var result = command.ExecuteScalar();
+            if (result != null && (int)result > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        bool UsernameIsAvailable()
+        {
+            using var command =
+                _dbContext.DataSource.CreateCommand(
+                    "SELECT id FROM users WHERE name=(@username)");
+            command.Parameters.AddWithValue("username", username.ToLower());
+            var result = command.ExecuteScalar();
+            if (result != null && (int)result > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        void CompleteRegistration()
+        {
+            using var command =
+                _dbContext.DataSource.CreateCommand(
+                    "INSERT INTO users (name, password) VALUES (@username, @password);" +
+                    "SELECT id FROM users WHERE name=(@username) AND password=(@password)");
+            command.Parameters.AddWithValue("username", username.ToLower());
+            command.Parameters.AddWithValue("password", password);
+            var newUserId = command.ExecuteScalar();
+
+            CreateUserSession((int)newUserId, username);
+        }
     }
 }
